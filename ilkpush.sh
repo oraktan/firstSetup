92714681fa1d382f0e2e4e7db981245dc88da9b0
bonvoyage <oya@casper.station>
2024-02-06 02:34:53 +0300

    vimrc files have been added

--- ilkpush.sh
+++ ilkpush.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 git add .      # Tüm dosyaları (dizin içeriğini) ekleyin
-git commit -m "İlk commit"  # Bir commit oluşturun
+git commit -m "vimrc files have been added "  # Bir commit oluşturun
 git push origin main  # Değişiklikleri GitHub'a yükleyin
+
+
+#ssh bağlantısının olup olmadığını söylüyor
+ssh -T git@github.com
+
+
+git remote set-url origin git@github.com:oraktan/firstSetup.git
 
